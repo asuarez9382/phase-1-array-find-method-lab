ec1ab14da485c570cd9348fe40655a491e83bc04
Adrian Suarez <asuarez9382@gmail.com>
2023-11-08 12:02:12 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(arr) {
+    //arr is array of objects
+    //each object has year and result property
+    //use find() to test if result === "W"
+    const obj = arr.find(element => element.result === "W")
+    if(obj){
+        return obj.year
+    }
+    else {
+        return undefined;
+    }
+}
